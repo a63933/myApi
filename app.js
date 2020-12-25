b0e3945ca5bb53d07423875ee830bb3d6569ce16
a63933 <1324147533@qq.com>
2020-12-25 19:35:45 +0800

    cors

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const testRouter = require('./router/test');
+var cors = require('cors');
 
 let app = express()
+app.use(cors())
 app.use('/test', testRouter)
 app.get('/getTime', (req, res) => {
   console.log(req.body)
